8b27d9481b15f3ab11cc27b92bca9761922cb38b
lh <1291191042@qq.com>
2021-05-09 00:56:52 +0800

    5-9

--- src/main/java/com/practice/Two.java
+++ src/main/java/com/practice/Two.java
@@ -1,9 +1,7 @@
 package com.practice;
 
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * 来自于One的补充，countDownLatch在多线程下的使用
@@ -102,6 +100,21 @@ class CachedThreadPoolTest{
         }
 
         executorService.shutdown();
+    }
+}
+
+/**
+ * 创建一个定长线程池，支持定时及周期性任务执行。延迟执行示例代码如下：
+ */
+class newScheduledThreadPoolTest{
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService=Executors.newScheduledThreadPool(5);
+        scheduledExecutorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("delay 3 seconds");
+            }
+        },3, TimeUnit.SECONDS);
     }
 }
 
